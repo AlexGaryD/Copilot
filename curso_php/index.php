362f0cabccb514200595fad936bd2893d7f28892
Alexis Vera S. <alxvcs93@hotmail.com>
2024-10-22 21:13:14 -0500

    modifique index. php

--- curso_php/index.php
+++ curso_php/index.php
@@ -3,3 +3,20 @@ echo "hola mundo <br>" ;
 $nombre = "Alexis"; 
 $apellido = "Vera";
 echo "Mi nombre es $nombre $apellido <br>";
+echo "El resultado de 2+2 es: " . (2+2) . "<br>";
+echo "El resultado de 2*2 es: " . (2*2) . "<br>";
+echo "El resultado de 2/2 es: " . (2/2) . "<br>";
+echo "El resultado de 2-2 es: " . (2-2) . "<br>";
+echo "El resultado de 2%2 es: " . (2%2) . "<br>";
+echo "El resultado de 2**2 es: " . (2**2) . "<br>";
+echo "El resultado de 2**3 es: " . (2**3) . "<br>";
+echo "El resultado de 2**4 es: " . (2**4) . "<br>";
+echo "El resultado de 2**5 es: " . (2**5) . "<br>";
+$personas = [
+    "Carlos" => 23,
+    "Alexis" => 24,
+    "Juan" => 25
+];
+var_dump($personas);
+echo "<br>";
+print_r($personas);
